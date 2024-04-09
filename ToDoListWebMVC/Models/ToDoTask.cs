e3d565fccd60d62ccc08abdfa8f59e34b84cf81d
gtadayukey <gtadayukey@gmail.com>
2024-04-09 18:29:59 -0300

    add ToDoTask object

---
+++ ToDoListWebMVC/Models/ToDoTask.cs
@@ -0,0 +1,20 @@
+﻿namespace ToDoListWebMVC.Models
+{
+    public class ToDoTask
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+
+        public ToDoTask() { }
+
+        public ToDoTask(string title, string description, DateOnly date, TimeOnly time)
+        {
+            Title = title;
+            Description = description;
+            Date = date;
+            Time = time;
+        }
+    }
+}
