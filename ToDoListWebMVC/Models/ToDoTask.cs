fa78e20f4e2b1ccd380a0f9cf984dc2ca2d61a61
gtadayukey <gtadayukey@gmail.com>
2024-04-10 16:41:29 -0300

    change some display attributes

--- ToDoListWebMVC/Models/ToDoTask.cs
+++ ToDoListWebMVC/Models/ToDoTask.cs
@@ -10,11 +10,17 @@ namespace ToDoListWebMVC.Models
         [Required(ErrorMessage = "{0} required")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Title { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No Description")]
         public string? Description { get; set; }
 
         [Required(ErrorMessage = "{0} required")]
+        [DataType(DataType.Date)]
         [FutureDate("Date")]
         public DateOnly Date { get; set; }
+
+        [DisplayFormat(NullDisplayText = "Without Time")]
+        [DataType(DataType.Time)]
         public TimeOnly? Time { get; set; }
 
         public ToDoTask() {}
